761b8699cc2c51633a1800306ed4c101f4d5e9e6
Gaurav Saini <44020369+sgauravk@users.noreply.github.com>
2019-04-02 16:36:29 +0530

    Updates

---
+++ precache-manifest.c98c7ae25a3f9e2e1a99335043f57ae1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e5a28b35369377fe73dd",
+    "url": "/static/js/main.7a650bbd.chunk.js"
+  },
+  {
+    "revision": "60b11684390a34760514",
+    "url": "/static/js/2.93d9075c.chunk.js"
+  },
+  {
+    "revision": "e5a28b35369377fe73dd",
+    "url": "/static/css/main.06f6f2d4.chunk.css"
+  },
+  {
+    "revision": "97f4f0afb0ec6caf894bacb91d0a4136",
+    "url": "/index.html"
+  }
+];
